6f5996c19d0523cb8ff4e5914201e201ee9e7e2b
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-10-15 00:52:15 -0300

    Criação do metodo de criar usuario comum

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,17 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegisterRequest;
+use App\Http\Resources\UserResource;
 use App\Models\User;
+use App\Services\UserService;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    private User $user;
+    private UserService $service;
 
-    public function __construct(User $user)
+    public function __construct(UserService $service)
     {
-        $this->user = $user;
+        $this->service = $service;
     }
 
+    public function store (RegisterRequest $request){
+        $user = $this->service->store($request);
+
+        if(!$user) return response(['error'=>'Usuario nao criado'], 400);
+
+        $resource = new UserResource($user);
+
+        return $resource->response()->setStatusCode(201);
+    }
 
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,4 +10,6 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('/validate-token', [AuthController::class, 'validateToken']);
+
+    Route::post('/register-user', [UserController::class, 'store']);
 });
