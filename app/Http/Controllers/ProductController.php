e23fed863e92149dae2a86444befb9a3cead9b19
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-10-25 00:06:57 -0300

    Criação do método de buscar todos os produtos

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\ProductService;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+    private ProductService $service;
 
+    public function __construct(ProductService $service) {
+        $this->service = $service;
+    }
+
+    public function index() {
+        $products = $this->service->getAll();
+
+        if(count($products) == 0) return response(['message'=>'nenhum produto encontrado'], 404);
+
+        return $products->response()->setStatusCode(200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,8 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
-use App\Models\Product;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
@@ -16,5 +15,5 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('/users', [UserController::class, 'store']);
     Route::delete('/users', [UserController::class, 'destroy']);
 
-    Route::get('/products', [Product::class, 'index']);
+    Route::get('/products', [ProductController::class, 'index']);
 });
