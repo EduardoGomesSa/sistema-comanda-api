ea93df56369eb408ec4610ef48e81899562aaa71
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-10-27 00:27:54 -0300

    Criação das colunas da migration de inventory

--- database/migrations/2024_10_27_025432_create_inventories_table.php
+++ database/migrations/2024_10_27_025432_create_inventories_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('inventories', function (Blueprint $table) {
             $table->id();
+            $table->integer('amount');
+            $table->foreignId('product_id')
+                ->constrained('products')
+                ->onUpdate('CASCADE')
+                ->onDelete('CASCADE');
             $table->timestamps();
         });
     }
